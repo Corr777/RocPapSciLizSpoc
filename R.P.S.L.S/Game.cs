b2568a8a5d4e4992a6a577fa882330aa6b553997
unknown <clw470@gmail.com>
2020-01-13 11:33:50 -0600

    corrected git file & updated classes to reflect inheritance

---
+++ R.P.S.L.S/Game.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R.P.S.L.S
+{ //Member Variables(Has A)
+
+
+    class Game
+    {
+
+
+        //Constructor (SPAWN)
+
+        public Game() 
+        {
+        
+        
+        
+        
+        
+        
+        }
+
+        //Can Do
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
